7e25f5c1ae9de55389f3a10a808a90b156d90e30
ajiks143 <ajiks143@IN2291710W2.ey.net>
2021-03-03 19:47:22 +0530

    Real time app

--- job/sample.py
+++ job/sample.py
@@ -1,9 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import StructType  
+import json
 
 spark = SparkSession.builder.master('local[*]').appName('sample').getOrCreate()
 
-df = spark.read.json("data/input/test.json").cache()
-json_schema = spark.read.json(df.rdd.map(lambda row: row.json)).schema
-#df.withColumn('json', from_json(col('json'), json_schema))
-json_schema.printSchema()
+df = spark.read.format("json").options(multiLine=True).load("data/input/test.json")
+schema_json = df.schema.json()
+print(schema_json)
+
+# Restore schema from json:
+
+new_schema = StructType.fromJson(json.loads(schema_json))
+print(new_schema)
--- job/spark_consumer.py
+++ job/spark_consumer.py
@@ -1,5 +1,5 @@
 from pyspark.sql.functions import *
-
+from pyspark.sql.types import *
 from dependencies.spark import start_spark
 
 def main():
@@ -22,7 +22,7 @@ def main():
 
     string_df = df.selectExpr("CAST(value AS STRING)")
 
-    json_schema = spark.read.json(string_df.rdd.map(lambda row: row.json)).schema
+    json_schema = StructType(List(StructField(block_hash,StringType,true),StructField(block_height,LongType,true),StructField(block_time,LongType,true),StructField(confirmations,LongType,true),StructField(fee,LongType,true),StructField(hash,StringType,true),StructField(inputs,ArrayType(StructType(List(StructField(prev_addresses,ArrayType(StringType,true),true),StructField(prev_position,LongType,true),StructField(prev_tx_hash,StringType,true),StructField(prev_type,StringType,true),StructField(prev_value,LongType,true),StructField(sequence,LongType,true))),true),true),StructField(inputs_count,LongType,true),StructField(inputs_value,LongType,true),StructField(is_coinbase,BooleanType,true),StructField(is_double_spend,BooleanType,true),StructField(is_sw_tx,BooleanType,true),StructField(lock_time,LongType,true),StructField(outputs,ArrayType(StructType(List(StructField(addresses,ArrayType(StringType,true),true),StructField(spent_by_tx,StringType,true),StructField(spent_by_tx_position,LongType,true),StructField(type,StringType,true),StructField(value,LongType,true))),true),true),StructField(outputs_count,LongType,true),StructField(outputs_value,LongType,true),StructField(sigops,LongType,true),StructField(size,LongType,true),StructField(version,LongType,true),StructField(vsize,LongType,true),StructField(weight,LongType,true),StructField(witness_hash,StringType,true)))
 
     json_df = string_df.withColumn("jsonData", from_json(col("value"), json_schema)).select("jsondata.*")
 
